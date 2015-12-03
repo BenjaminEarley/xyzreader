641634fcee43767e89c0b65603198cb3bba66abd
Benjamin Earley <bearley@outsitenetworks.com>
2015-12-03 15:56:09 -0500

    Library updates

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.xyzreader.ui;
 
+import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.LoaderManager;
@@ -89,6 +90,7 @@ public class ArticleDetailActivity extends ActionBarActivity
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             mUpButtonContainer.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
                 @Override
+                @TargetApi(Build.VERSION_CODES.LOLLIPOP)
                 public WindowInsets onApplyWindowInsets(View view, WindowInsets windowInsets) {
                     view.onApplyWindowInsets(windowInsets);
                     mTopInset = windowInsets.getSystemWindowInsetTop();
